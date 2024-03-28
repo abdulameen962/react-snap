78633d1dcd29cc81e02cb23feb5216cac9ff7285
abdulameen962 <77135492+abdulameen962@users.noreply.github.com>
2024-03-28 13:39:03 +0100

    Update puppeteer_utils.js

--- src/puppeteer_utils.js
+++ src/puppeteer_utils.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const _ = require("highland");
 const url = require("url");
 const mapStackTrace = require("sourcemapped-stacktrace-node").default;
